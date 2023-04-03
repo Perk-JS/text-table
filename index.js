3c47ebe180af6d4c14fef388554f8c97222fbf7b
Ilya Karpuk <perkovec24@gmail.com>
2023-04-04 01:06:46 +0300

    Added v1.1.0 benchmark

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const textTable = require('text-table');
 const perkTextTableGit = require('perkjs-text-table-git');
 const perkTextTable100 = require('perkjs-text-table-100');
+const perkTextTable110 = require('perkjs-text-table-110');
 const Benchmark = require('benchmark');
 
 const bench = new Benchmark.Suite;
@@ -17,6 +18,7 @@ const options = { align: [ 'l', '.' ] };
 
 bench.add('Original text-table', () => textTable(template, options));
 bench.add('@perkjs/text-table master branch', () => perkTextTableGit(template, options));
+bench.add('@perkjs/text-table v1.1.0', () => perkTextTable110(template, options));
 bench.add('@perkjs/text-table v1.0.0', () => perkTextTable100(template, options));
 bench.on('cycle', (event) => console.log(String(event.target)));
 bench.run();
